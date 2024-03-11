1e23ebefa7bc08521849625dd4f0a541ab53b2b2
Vinijs <vini-spider-6@hotmail.com>
2024-03-11 01:14:55 -0300

    Aula 04 - 10/03/2024

--- testes/TorneSe.ServicoLancamentoNotas.Testes/Dominio/Entidades/NotaTestes.cs
+++ testes/TorneSe.ServicoLancamentoNotas.Testes/Dominio/Entidades/NotaTestes.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using TorneSe.ServicoLancamentoNotas.Dominio.Constantes;
 using TorneSe.ServicoLancamentoNotas.Dominio.Entidades;
 using TorneSe.ServicoLancamentoNotas.Dominio.Enums;
+using TorneSe.ServicoLancamentoNotas.Dominio.Exceptions;
 using Xunit;
 
 namespace TorneSe.ServicoLancamentoNotas.Testes.Dominio.Entidades;
@@ -18,7 +20,8 @@ public class NotaTestes
             AtividadeId = 34566,
             ValorNota = 10.00,
             DataLancamento = DateTime.Now,
-            UsuarioId = 34566
+            UsuarioId = 34566,
+            DataAtualizacao = DateTime.Now,
         };
 
         //Act
@@ -32,14 +35,130 @@ public class NotaTestes
         Assert.Equal(valoresEntrada.ValorNota, nota.ValorNota);
         Assert.Equal(valoresEntrada.DataLancamento, nota.DataLancamento);
         Assert.NotEqual(default(DateTime), nota.DataLancamento);
+        Assert.NotEqual(default(DateTime), nota.DataAtualizacao);
         Assert.Equal(valoresEntrada.UsuarioId, nota.UsuarioId);
         Assert.False(nota.CanceladaPorRetentativa);
         Assert.Equal(StatusIntegracao.AguardandoIntegracao, nota.StatusIntegracao);
         Assert.Null(nota.MotivoCancelamento);
     }
 
-    [Fact(DisplayName = "")]
+    [Theory(DisplayName = nameof(InstanciarNota_QuandoValorNotaInvalido_DeveLancarExcecao))]
     [Trait("Dominio", "Nota - Agregado")]
+    [InlineData(-1)]
+    [InlineData(11)]
+    public void InstanciarNota_QuandoValorNotaInvalido_DeveLancarExcecao(double valorNota)
+    {
+        //Arrange
+        var valoresEntrada = new
+        {
+            AlunoId = 1234,
+            AtividadeId = 34566,
+            ValorNota = valorNota,
+            DataLancamento = DateTime.Now,
+            UsuarioId = 34566,
+            DataAtualizacao = DateTime.Now,
+        };
+
+        //Act
+        var action = () => new Nota(valoresEntrada.AlunoId, valoresEntrada.AtividadeId,
+                            valoresEntrada.ValorNota, valoresEntrada.DataLancamento, valoresEntrada.UsuarioId);
+
+        var exception = Assert.Throws<ValidacaoEntidadeException>(action);
+        Assert.NotNull(exception);
+        Assert.Equal(ConstantesDominio.MensagemValidacoes.ERRO_VALOR_NOTA_INVALIDO, exception.Message);
+
+        //Assert
+
+    }
+
+    [Theory(DisplayName = nameof(InstanciarNota_QuandoUsuarioIdInvalido_DeveLancarExcecao))]
+    [Trait("Dominio", "Nota - Agregado")]
+    [InlineData(-1)]
+    [InlineData(0)]
+    public void InstanciarNota_QuandoUsuarioIdInvalido_DeveLancarExcecao(int usuarioId)
+    {
+        //Arrange
+        var valoresEntrada = new
+        {
+            AlunoId = 1234,
+            AtividadeId = 34566,
+            ValorNota = 10,
+            DataLancamento = DateTime.Now,
+            UsuarioId = usuarioId,
+            DataAtualizacao = DateTime.Now,
+        };
+
+        //Act
+        var action = () => new Nota(valoresEntrada.AlunoId, valoresEntrada.AtividadeId,
+                            valoresEntrada.ValorNota, valoresEntrada.DataLancamento, valoresEntrada.UsuarioId);
+
+        var exception = Assert.Throws<ValidacaoEntidadeException>(action);
+        Assert.NotNull(exception);
+        Assert.Equal(ConstantesDominio.MensagemValidacoes.ERRO_USUARIO_INVALIDO, exception.Message);
+
+        //Assert
+
+    }
+
+    [Theory(DisplayName = nameof(InstanciarNota_QuandoAlunoIdInvalido_DeveLancarExcecao))]
+    [Trait("Dominio", "Nota - Agregado")]
+    [InlineData(-1)]
+    [InlineData(0)]
+    public void InstanciarNota_QuandoAlunoIdInvalido_DeveLancarExcecao(int alunoId)
+    {
+        //Arrange
+        var valoresEntrada = new
+        {
+            AlunoId = alunoId,
+            AtividadeId = 34566,
+            ValorNota = 10,
+            DataLancamento = DateTime.Now,
+            UsuarioId = 34566,
+            DataAtualizacao = DateTime.Now,
+        };
+
+        //Act
+        var action = () => new Nota(valoresEntrada.AlunoId, valoresEntrada.AtividadeId,
+                            valoresEntrada.ValorNota, valoresEntrada.DataLancamento, valoresEntrada.UsuarioId);
+
+        var exception = Assert.Throws<ValidacaoEntidadeException>(action);
+        Assert.NotNull(exception);
+        Assert.Equal(ConstantesDominio.MensagemValidacoes.ERRO_ALUNO_INVALIDO, exception.Message);
+
+        //Assert
+
+    }
+
+    [Theory(DisplayName = nameof(InstanciarNota_QuandoAtividadeIdInvalido_DeveLancarExcecao))]
+    [Trait("Dominio", "Nota - Agregado")]
+    [InlineData(-1)]
+    [InlineData(0)]
+    public void InstanciarNota_QuandoAtividadeIdInvalido_DeveLancarExcecao(int atividadeId)
+    {
+        //Arrange
+        var valoresEntrada = new
+        {
+            AlunoId = 1234,
+            AtividadeId = atividadeId,
+            ValorNota = 10,
+            DataLancamento = DateTime.Now,
+            UsuarioId = 34566,
+            DataAtualizacao = DateTime.Now,
+        };
+
+        //Act
+        var action = () => new Nota(valoresEntrada.AlunoId, valoresEntrada.AtividadeId,
+                            valoresEntrada.ValorNota, valoresEntrada.DataLancamento, valoresEntrada.UsuarioId);
+
+        var exception = Assert.Throws<ValidacaoEntidadeException>(action);
+        Assert.NotNull(exception);
+        Assert.Equal(ConstantesDominio.MensagemValidacoes.ERRO_ATIVIDADE_INVALIDA, exception.Message);
+
+        //Assert
+
+    }
+
     //Preciso controlar a nota lançada já foi integrada
     //Caso uma nota venha a ser cancelada preciso de um motivo para o cancelamento
+    //Um valor de nota deve estar no intervalo entre 0 a 10
 }
