e9acaa59ac6b62c8e1d8fcfe0b7e6562fb4e779f
Vinijs <vini-spider-6@hotmail.com>
2024-04-11 21:18:53 -0300

    Aula 25 - 10/04/2024

---
+++ src/TorneSe.ServicoLancamentoNotas.Aplicacao/Validacoes/LancarNotaInputValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using TorneSe.ServicoLancamentoNotas.Aplicacao.CasosDeUsos.Nota.Lancar.DTOs;
+using TorneSe.ServicoLancamentoNotas.Aplicacao.Constantes;
+
+namespace TorneSe.ServicoLancamentoNotas.Aplicacao.Validacoes;
+
+public class LancarNotaInputValidator : AbstractValidator<LancarNotaInput>
+{
+    public readonly static LancarNotaInputValidator Instance = new();  
+    public LancarNotaInputValidator()
+    {
+        RuleFor(x => x.AlunoId)
+            .GreaterThan(default(int))
+            .WithMessage(ConstantesAplicacao.MensagensValidacao.ALUNO_ID_INVALIDO);
+
+        RuleFor(x => x.ProfessorId)
+            .GreaterThan(default(int))
+            .WithMessage(ConstantesAplicacao.MensagensValidacao.PROFESSOR_ID_INVALIDO);
+
+        RuleFor(x => x.AtividadeId)
+            .GreaterThan(default(int))
+            .WithMessage(ConstantesAplicacao.MensagensValidacao.ATIVIDADE_ID_INVALIDO);
+
+        RuleFor(x => x.ValorNota)
+            .GreaterThan(default(double))
+            .WithMessage(ConstantesAplicacao.MensagensValidacao.NOTA_INVALIDA);
+    }
+}
---
+++ testes/TorneSe.ServicoLancamentoNotas.Testes/Aplicacao/Behaviors/LancarNotaInputValidacaoBehaviorTests.cs
@@ -0,0 +1,75 @@
+﻿using FluentAssertions;
+using FluentValidation;
+using Moq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TorneSe.ServicoLancamentoNotas.Aplicacao.Behaviors;
+using TorneSe.ServicoLancamentoNotas.Aplicacao.CasosDeUsos.Nota.Lancar.DTOs;
+using TorneSe.ServicoLancamentoNotas.Aplicacao.Enums;
+using TorneSe.ServicoLancamentoNotas.Aplicacao.Validacoes;
+using Xunit;
+
+namespace TorneSe.ServicoLancamentoNotas.Testes.Aplicacao.Behaviors;
+
+[Collection(nameof(LancarNotaInputValidacaoBehaviorTestsFixture))]
+public class LancarNotaInputValidacaoBehaviorTests
+{
+    private readonly LancarNotaInputValidacaoBehaviorTestsFixture _fixture;
+    private readonly Mock<IValidator<LancarNotaInput>> _validadorMock;
+    private LancarNotaInputValidacaoBehavior _sut;
+
+    public LancarNotaInputValidacaoBehaviorTests(LancarNotaInputValidacaoBehaviorTestsFixture fixture)
+    {
+        _fixture = fixture;
+        _validadorMock = new Mock<IValidator<LancarNotaInput>>();
+        _sut = new LancarNotaInputValidacaoBehavior(_validadorMock.Object);
+    }
+
+    [Fact(DisplayName = nameof(Handle_QuandoValidacaoPossuiErros_DeveRetornarErro))]
+    [Trait("Aplicacao", "Nota - Comportamentos")]
+    public async Task Handle_QuandoValidacaoPossuiErros_DeveRetornarErro()
+    {
+        var request = _fixture.DevolveNotaInputInValido();
+        var resultadoValidacao = await LancarNotaInputValidator.Instance.ValidateAsync(request);
+        _validadorMock.Setup(x => x.ValidateAsync(It.IsAny<LancarNotaInput>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(resultadoValidacao);
+
+        var resultado = await _sut.Handle(request, null!, CancellationToken.None);
+
+        resultado.Should().NotBeNull();
+        resultado.Erro.Should().Be(TipoErro.InputNotaInvalido);
+        resultado.Sucesso.Should().BeFalse();
+    }
+
+    [Fact(DisplayName = nameof(Handle_QuandoValidacaoPossuiErros_DeveRetornarSucesso))]
+    [Trait("Aplicacao", "Nota - Comportamentos")]
+    public async Task Handle_QuandoValidacaoPossuiErros_DeveRetornarSucesso()
+    {
+        var request = _fixture.DevolveNotaInputValido();
+        var resultadoValidacao = await LancarNotaInputValidator.Instance.ValidateAsync(request);
+        _validadorMock.Setup(x => x.ValidateAsync(It.IsAny<LancarNotaInput>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(resultadoValidacao);
+
+
+        var resultado = await _sut.Handle(request, _fixture.RetornaSucesso, CancellationToken.None);
+
+        resultado.Should().NotBeNull();
+        resultado.Sucesso.Should().BeTrue();
+        resultado.Dado.Should().NotBeNull();
+    }
+
+    [Fact(DisplayName = nameof(Handle_QuandoValidadorEstaNulo_DeveRetornarSucesso))]
+    [Trait("Aplicacao", "Nota - Comportamentos")]
+    public async Task Handle_QuandoValidadorEstaNulo_DeveRetornarSucesso()
+    {
+        var request = _fixture.DevolveNotaInputValido();
+        _sut = new LancarNotaInputValidacaoBehavior(null!);
+
+        var resultado = await _sut.Handle(request, _fixture.RetornaSucesso, CancellationToken.None);
+
+        resultado.Should().NotBeNull();
+        resultado.Sucesso.Should().BeTrue();
+        resultado.Dado.Should().NotBeNull();
+    }
+}
